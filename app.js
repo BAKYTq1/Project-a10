4829fd395ac02d3c88cd38d9613f853c70609394
mirbek10 <m51869956@gmail.com>
2025-01-08 18:03:03 +0600

    last section

---
+++ app.js
@@ -0,0 +1,11 @@
+const carousel = document.getElementById('carousel');
+    const nextButton = document.getElementById('next');
+    const prevButton = document.getElementById('prev');
+
+    nextButton.addEventListener('click', () => {
+      carousel.scrollBy({ left: carousel.offsetWidth, behavior: 'smooth' });
+    });
+
+    prevButton.addEventListener('click', () => {
+      carousel.scrollBy({ left: -carousel.offsetWidth, behavior: 'smooth' });
+    });
