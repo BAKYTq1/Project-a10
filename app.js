ad86c3c7791a7d15512ce0ba34b2acaf7390bf5f
mirbek10 <m51869956@gmail.com>
2025-01-09 15:44:48 +0600

    section 6

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const carousel = document.getElementById('carousel');
-    const nextButton = document.getElementById('next');
-    const prevButton = document.getElementById('prev');
+  const carousel = document.getElementById('carousel');
+  const nextButton = document.getElementById('next');
+  const prevButton = document.getElementById('prev');
 
-    nextButton.addEventListener('click', () => {
-      carousel.scrollBy({ left: carousel.offsetWidth, behavior: 'smooth' });
-    });
+  // Установим ширину прокрутки на половину ширины карточки
+  const scrollAmount = document.querySelector('.flex-shrink-0').offsetWidth / 2;
 
-    prevButton.addEventListener('click', () => {
-      carousel.scrollBy({ left: -carousel.offsetWidth, behavior: 'smooth' });
-    });
+  nextButton.addEventListener('click', () => {
+    carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  });
+
+  prevButton.addEventListener('click', () => {
+    carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  });
